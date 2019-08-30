a036ea5256b5b7abb8917e8627ff125ba8b7d73d
Sanek <djShtaket88@mail.ru>
2019-08-30 20:11:36 +0300

    task #1776: fix VerifyEmail

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -45,7 +45,7 @@ class VerifyEmail extends Notification
                     ->line('Подтвердите адрес электронной почты.')
                     ->action('Подтвердить', $verificationUrl)
                     ->line('Если Вы не создавали учётную запись, то никаких дальнейших действий не требуется.')
-                    ->view('emails.verified');
+                    ->markdown('emails.verified');
     }
 
     /**
