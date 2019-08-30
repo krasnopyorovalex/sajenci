a6de317a7fab1ab02be92514cefabeb8dedc7a8d
Sanek <djShtaket88@mail.ru>
2019-08-30 20:31:24 +0300

    task #1776: fix VerifyEmail

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Regards' => 'Regards',
+    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+    'into your web browser: [:actionURL](:actionURL)' => "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+        'into your web browser: [:actionURL](:actionURL)'
+
+];
