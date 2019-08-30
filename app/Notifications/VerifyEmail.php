d5fd568b3c91d52e005dd99a15fb32ed5ec7a4b6
Sanek <djShtaket88@mail.ru>
2019-08-30 19:16:23 +0300

    task #1776: change VerifyEmail notification

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -50,7 +50,9 @@ class VerifyEmail extends Notification
         }
 
         return (new MailMessage)
+                    ->theme('Верификация email-адреса')
                     ->from('info@sajenci-krym.ru')
+                    ->greeting('Здравствуйте!')
                     ->line('Подтвердите адрес электронной почты.')
                     ->action('Подтвердить', $verificationUrl)
                     ->line('Если Вы не создавали учётную запись, то никаких дальнейших действий не требуется.');
