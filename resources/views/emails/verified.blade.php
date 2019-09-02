1036f4a6744ae8eaff72ad6af5b9fd1236af4ad9
Sanek <djShtaket88@mail.ru>
2019-09-02 16:41:00 +0300

    task #1776: fix VerifyEmail view

--- resources/views/emails/verified.blade.php
+++ resources/views/emails/verified.blade.php
@@ -1,8 +1,28 @@
 @component('mail::message')
 
-    {{-- Action Button --}}
-    @component('mail::button', ['url' => $verificationUrl, 'color' => 'primary'])
-        Подтвердить
-    @endcomponent
+# Здравствуйте!
 
+Подтвердите адрес электронной почты.
+
+{{-- Action Button --}}
+@component('mail::button', ['url' => $verificationUrl, 'color' => 'primary'])
+Подтвердить
+@endcomponent
+
+Если Вы не создавали учётную запись, то никаких дальнейших действий не требуется.
+
+С уважением,
+«Саженцы в Крыму»
+
+{{-- Subcopy --}}
+@slot('subcopy')
+    @lang(
+        "Если у вас возникли проблемы, нажав кнопку \":actionText\", скопируйте и вставьте URL ниже\n".
+        'в Ваш браузер: [:actionURL](:actionURL)',
+        [
+            'actionText' => 'Подтвердить',
+            'actionURL' => $verificationUrl,
+        ]
+    )
+@endslot
 @endcomponent
