d23ec62b1617b7f9c21de75fcd4a1667264f9b57
Sanek <djShtaket88@mail.ru>
2019-09-03 11:50:07 +0300

    task #1776: add to ForgotPasswordController Notifiable

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Notifications\Notifiable;
 
 class ForgotPasswordController extends Controller
 {
+
+    use Notifiable;
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
@@ -28,5 +33,16 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new ResetPasswordNotification($token));
     }
 }
